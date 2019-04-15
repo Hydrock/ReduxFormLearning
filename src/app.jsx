6ddcb4df4e4d810d1d581e6ca24a6b443353241e
Вечканов Алексей Сергеевич <AVechkanov@alfabank.ru>
2019-04-15 11:14:54 +0300

    add test reducer

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,10 @@ const rootReducer = combineReducers({
     testReducerStore: testReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
     buttonHandler = (store) => {
